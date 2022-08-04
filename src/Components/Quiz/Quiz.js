59a6ef4c5f1f0526629ba8ebc189cb2981885bc2
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-08-04 14:56:39 -0600

    Add initial results page

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -2,39 +2,39 @@
 import React from "react"
 // import { BrowserRouter } from "react-dom"
 import './Quiz.css';
-// import Results from "../Results/Results"
+import Results from "../Results/Results"
 // import External from "../External/External"
 
 const Quiz = ({questionData}) => {
-console.log("QuestionDATA", questionData);
+// console.log("QuestionDATA", questionData);
 
-    const getReducedQs = questionData.reduce((reducedQs, q) => {
-        if ((q.category.includes("Animals")) && (!q.question.includes("&quot")) && (!q.question.includes("#"))) {
-            reducedQs.push(q)
-        } else if ((q.category.includes("Books")) && (!q.question.includes("&quot")) && (!q.question.includes("#"))) {
-            reducedQs.push(q)
-        } else if ((q.category.includes("Science")) && (!q.question.includes("&quot")) && (!q.question.includes("#"))) {
-            reducedQs.push(q)
-        } else if ((q.category.includes("General")) && (!q.question.includes("&quot")) && (!q.question.includes("#"))) {
-            reducedQs.push(q)
-        } else if ((q.category.includes("Geography")) && (!q.question.includes("&quot")) && (!q.question.includes("#"))) {
-            reducedQs.push(q)
-        }
-    console.log("reducedQs", reducedQs)
-    return (<Game key={reducedQs.question} category={reducedQs.category} question={reducedQs.question} answer={reducedQs.correct_answer}/>)
-    
-}, [])
+//     const getReducedQs = questionData.reduce((reducedQs, q) => {
+//         if ((q.category.includes("Animals")) && (!q.question.includes("&quot")) && (!q.question.includes("#"))) {
+//             reducedQs.push(q)
+//         } else if ((q.category.includes("Books")) && (!q.question.includes("&quot")) && (!q.question.includes("#"))) {
+//             reducedQs.push(q)
+//         } else if ((q.category.includes("Science")) && (!q.question.includes("&quot")) && (!q.question.includes("#"))) {
+//             reducedQs.push(q)
+//         } else if ((q.category.includes("General")) && (!q.question.includes("&quot")) && (!q.question.includes("#"))) {
+//             reducedQs.push(q)
+//         } else if ((q.category.includes("Geography")) && (!q.question.includes("&quot")) && (!q.question.includes("#"))) {
+//             reducedQs.push(q)
+//         }
+//     console.log("reducedQs", reducedQs)
+//     return (<Game key={reducedQs.question} category={reducedQs.category} question={reducedQs.question} answer={reducedQs.correct_answer}/>)
+// }, [])
 
     return(
         <div className="quizComponentDiv">
             {/* <img className="backArrow" alt="arrow pointing left to indicate a return to the previous page" src=""/>
             <img className="forwardArrow" alt="arrow pointing right to navigate to the next page" src="" /> */}
             <div className="questionDiv">
-                <h2 className="question">{reducedQs.question}</h2>
+                <h2 className="question">What is a question here?</h2>
                 <button className="true">TRUE</button>
                 <button className="false">FALSE</button>
                 <p className="progress">8/10</p>
             </div>
+            <Results/>
         </div>
     )
 }
--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,6 +1,36 @@
-// import { useState, useEffect } from "react";
 // import { BrowserRouter } from "react-dom"
-// import './Results.css';
+import './Results.css';
 // import External from "../External/External"
+import { fetchLaureates } from "../../apiCalls"
+import { useState, useEffect } from "react"
+
+const Results = () => {
+    const [laureateData, setLaureateData] = useState([])
+
+    useEffect(()=> {
+        fetchLaureates()
+        .then(data => {
+        console.log("data", data.laureates)
+        setLaureateData(data.laureates)
+      })
+    }, [])
+
+    return(
+        <div className="resultsComponentDiv">
+            <div className="matchDetails">
+                <h3 className="matchName">based on your results, you match with <span>Michael A. Spence</span></h3>
+                <p className="matchCategory">Recipient of the YEAR Science and Nature Prize. </p>
+                <p className="matchQuote">"for his work on fghdgghg kfh dfjghjdfgdhjfg jhsg dhjg  ghjkhjgd f jghd."</p>
+            </div>
+            <div className="studentDetails">
+                <p className="correctCategories">You answered correct questions in the following categories: "Geography" , "Science and Nature"</p>
+                <p className="correctCategories">It looks like we have found the next Nobel laureate in CATEGORY in You!</p>
+                <p>one of 6 icons here </p>
+                <button classNAme="retakeButton">Re-take quiz</button>
+            </div>
+        </div>
+    )
+}
+
 
 export default Results
