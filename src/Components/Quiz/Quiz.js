2ecd186684c593c5271d9641c8a9233725b261b5
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-08-03 21:14:53 -0600

    Begin css for quiz component

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,16 +1,16 @@
 // import { useState, useEffect } from "react";
 import React from "react"
 // import { BrowserRouter } from "react-dom"
-// import './Quiz.css';
+import './Quiz.css';
 // import Results from "../Results/Results"
 // import External from "../External/External"
 
-const Quiz = () => {
-
+const Quiz = ({questionData}) => {
+console.log("QuestionDATA", questionData);
     return(
         <div className="quizComponentDiv">
-            <img className="backArrow" alt="arrow pointing left to indicate a return to the previous page" src=""/>
-            <img className="forwardArrow" alt="arrow pointing right to navigate to the next page" src="" />
+            {/* <img className="backArrow" alt="arrow pointing left to indicate a return to the previous page" src=""/>
+            <img className="forwardArrow" alt="arrow pointing right to navigate to the next page" src="" /> */}
             <div className="questionDiv">
                 <h2 className="question">Question Here is this where?</h2>
                 <button className="true">TRUE</button>
