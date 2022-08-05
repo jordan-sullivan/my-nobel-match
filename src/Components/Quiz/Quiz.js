85bc3a716a562a81949b87b4e1ba4dc7410afda2
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-08-04 21:49:00 -0600

    Add correct vs incorrect answers into setResults array when clicking true or false

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -4,33 +4,30 @@ import Results from "../Results/Results"
 
 
 const Quiz = ({ questions }) => {
-    console.log("QUESTIONS", questions);
     const [counter, setCounter] = useState(0)
-    const [selected, setSelected] = useState(true)
     const [results, setResults] = useState([])
     
-    // onclick(() => {
-    //     console.log("OnClick working")
-    // })
     
-    const checkCorrect = (event) => {
+    const checkTrueCorrect = (event) => {
         event.preventDefault()
-        console.log("checkCorrect working")
-    //  When True or False selected, check against correct_answer and if correct, push that Qs category into array
-    //setResults
-    // increaseCounter()
-}
-
-const increaseCounter = () => {
-    console.log("increaseCounter working")
-    if(counter === 10) {
-
+        if(questions[counter].correct_answer === "True"){
+            setResults(results => [...results, questions[counter].category])
+        } else {
+            setResults(results => [...results, "incorrect"])
+        }
     }
-    //  Add Next Question that will act as a submit Button and adds one to functionality to it to counter.
-    setCounter(counter +1)
-    //now go to next page
-    //  If counter is at 10, render "See my results" page instead.(Results container ?) That will link to the results page.to display info.
-}
+
+    const checkFalseCorrect = (event) => {
+        if(questions[counter].correct_answer === "False") {
+            setResults(results => [...results, questions[counter].category])
+        } else {
+            setResults(results => [...results, "incorrect"])
+        }
+    }
+
+    const increaseCounter = () => {
+        setCounter(counter +1)
+    }
 
     
     return(
@@ -39,8 +36,9 @@ const increaseCounter = () => {
             {counter < 10 ? ( <>
         <div className="questionDiv">
             <h2 className="question">{questions[counter].question}</h2>
-            <button className="true">TRUE</button>
-            <button className="false">FALSE</button>
+            <p className="questionCategory">{questions[counter].category}</p>
+            <button className="true" onClick={(event) => checkTrueCorrect(event)}>TRUE</button>
+            <button className="false" onClick={(event) => checkFalseCorrect(event)}>FALSE</button>
             <p className="progress">{counter}/10</p>
             <button className="nextQuestionButton" onClick={increaseCounter}>Next Question</button>
         </div>
