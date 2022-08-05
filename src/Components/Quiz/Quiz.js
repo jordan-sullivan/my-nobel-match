1d5f97413d0ba6b3c256c1068ffb1f51373f1bf8
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-08-04 19:42:09 -0600

    Add increaseCounter function to count questions and move to next question

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -6,17 +6,39 @@ import Results from "../Results/Results"
 const Quiz = ({ questions }) => {
     console.log("QUESTIONS", questions);
     const [counter, setCounter] = useState(0)
+    const [selected, setSelected] = useState(true)
     const [results, setResults] = useState([])
+    
+    // onclick(() => {
+    //     console.log("OnClick working")
+    // })
+    
+    const checkCorrect = (event) => {
+        event.preventDefault()
+        console.log("checkCorrect working")
+    //  When True or False selected, check against correct_answer and if correct, push that Qs category into array
+    //setResults
+    // increaseCounter()
+}
+
+const increaseCounter = () => {
+    console.log("increaseCounter working")
+    //  Add Next Question that will act as a submit Button and adds one to functionality to it to counter.
+    setCounter(counter +1)
+    //now go to next page
+    //  If counter is at 10, render "See my results" page instead.(Results container ?) That will link to the results page.to display info.
+}
 
     
     return(
         <div className="quizComponentDiv">     
     {questions.length ? ( <>
-        <div className="gameDiv">
+        <div className="questionDiv">
             <h2 className="question">{questions[counter].question}</h2>
             <button className="true">TRUE</button>
             <button className="false">FALSE</button>
-            <p className="progress">{counter}/{questions.length}</p>
+            <p className="progress">{counter}/10</p>
+            <button className="nextQuestionButton" onClick={increaseCounter}>Next Question</button>
         </div>
     </>)
     : null }
